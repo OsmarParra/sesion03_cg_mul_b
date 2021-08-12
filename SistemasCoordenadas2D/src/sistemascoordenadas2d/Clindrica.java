12d3f98d5877b930c8cbdf94ac8c71ad19c72a03
OsmarParra <pipepamu@gmail.com>
2021-08-12 16:15:20 -0500

    Signed-off-by: OsmarParra <pipepamu@gmail.com>

---
+++ SistemasCoordenadas2D/src/sistemascoordenadas2d/Clindrica.java
@@ -0,0 +1,45 @@
+
+package sistemascoordenadas2d;
+
+
+public class Clindrica {
+ private double radio;
+private double angulo; 
+private double z;
+
+public Clindrica(){
+}
+public Clindrica (double radio, double angulo, double z){
+Clindrica obj = new Clindrica();
+this.raddio = radio;
+this.angulo = angulo;
+this.Z=z;
+}
+}
+public double getRadio(){
+return radio;
+}
+public double getAngulo(){
+return angulo;
+}
+public double getZ(){
+return z;
+}
+public void setRadio(double radio){
+this.radio = radio;
+}
+public void setAngulo(double angulo){
+this.angulo = angulo;
+} 
+public void setZ(double z){
+this.z = z;
+} 
+
+public Clindrica esfericas_cilindricas(double r, double angulox, doubley){
+Clindrica obj = new Clindrica();
+radio=1;
+angulo=1;
+z=1;
+return obj;
+}
+}
